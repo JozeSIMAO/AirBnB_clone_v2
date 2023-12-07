8030ddf352171a22eab9dfd23372647d74739373
JozeSIMAO <jozesimao10@gmail.com>
2023-12-07 14:25:18 +0200

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Fabric script to deploy a compressed archive to web servers"""
 
-from fabric.api import env, put, run
-from os.path import exists, join
-
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['100.27.13.53', '100.25.37.138']
 
 
@@ -15,22 +14,16 @@ def do_deploy(archive_path):
     try:
         archive_name = archive_path.split("/")[-1]
         no_ext_name = archive_name.split(".")[0]
-        remote_path = "/data/web_static/releases/{}/".format(no_ext_name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(remote_path))
-        run("tar -xzf /tmp/{} -C {}".format(archive_name, remote_path))
-
-        # Check if 'web_static' directory exists inside the archive
-        if not exists(join(remote_path, 'web_static')):
-            return False
-
-        run("rm /tmp/{}".format(archive_name))
-        run("mv {}web_static/* {}".format(remote_path, remote_path))
-        run("rm -rf {}web_static".format(remote_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(remote_path))
-
+        path = "/data/web_static/releases/"
+        
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext_name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_name, path, no_ext_name))
+        run('rm /tmp/{}'.format(archive_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext_name))
+        run('rm -rf {}{}/web_static'.format(path, no_ext_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext_name))
         return True
-    except Exception as e:
+    except:
         return False
