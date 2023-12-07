480867f8a07eaccfcec5f32d23b1a2efee4887d1
JozeSIMAO <jozesimao10@gmail.com>
2023-12-07 13:42:15 +0200

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,22 @@
 #!/usr/bin/env bash
-
-# Install Nginx if not already installed
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-# Create required directories
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create a fake HTML file
-echo "<html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-nginx_config="/etc/nginx/sites-available/default"
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/; }' "$nginx_config"
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
