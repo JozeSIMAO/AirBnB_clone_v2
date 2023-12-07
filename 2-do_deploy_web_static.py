7f6fe9de85f5f6a1b511c792005db78101404143
JozeSIMAO <jozesimao10@gmail.com>
2023-12-07 14:29:18 +0200

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,15 +15,17 @@ def do_deploy(archive_path):
         archive_name = archive_path.split("/")[-1]
         no_ext_name = archive_name.split(".")[0]
         path = "/data/web_static/releases/"
-        
+
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_name, path, no_ext_name))
+        run('tar -xzf /tmp/{} -C {}{}/'.
+            format(archive_name, path, no_ext_name))
         run('rm /tmp/{}'.format(archive_name))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext_name))
         run('rm -rf {}{}/web_static'.format(path, no_ext_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext_name))
+        run('ln -s {}{}/ /data/web_static/current'.
+            format(path, no_ext_name))
         return True
-    except:
+    except Exception as e:
         return False
