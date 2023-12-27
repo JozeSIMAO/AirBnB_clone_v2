198dde3f4b74b22bf2b086b50d125603f1951c5a
JozeSIMAO <jozesimao10@gmail.com>
2023-12-27 15:01:03 +0200

    python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,30 +8,26 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
-    return "HBNB"
-
+    """returns HBNB"""
+    return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """Displays 'C ' followed by the value of the text variable"""
-    return "C {}".format(escape(text.replace('_', ' ')))
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
-
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """Displays 'Python ' followed by the value of the text variable"""
-    return "Python {}".format(escape(text.replace('_', ' ')))
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
